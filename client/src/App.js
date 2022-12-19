90b220c1110c60594d46bf752564312f9250d50c
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-12-19 21:55:09 +0545

    React Page Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Login, Register } from "./pages";
 function App() {
-  return <h1>React App</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+import Login from "./Login";
+import Register from "./Register";
+
+export { Login, Register };
