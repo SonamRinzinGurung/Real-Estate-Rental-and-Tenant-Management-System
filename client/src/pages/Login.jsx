7a55ade0d675c846a891b9300a7251998911c38b
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-12-21 16:01:36 +0545

    Login and Register from Frontend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import FormRow from "../components/FormRow";
+import axios from "axios";
 
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const userInfo = {
+      email: e.target[0].value,
+      password: e.target[1].value,
+      role: "owner",
+    };
+    login(userInfo);
+  };
+
+  const login = async (userInfo) => {
+    try {
+      const { data } = await axios.post("/auth/login", userInfo);
+      const { owner, token } = data;
+      console.log(owner, token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
-      <form className="bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4 hover:shadow-xl w-96">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4 hover:shadow-xl w-96"
+      >
         <h1 className="font-bold text-4xl text-gray-800 font-mono text-center my-4 py-5">
           Login
         </h1>
@@ -15,7 +40,7 @@ const Login = () => {
         <div className="flex items-center justify-center">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
           >
             Sign In
           </button>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,44 @@
-import React from "react";
+import { useState } from "react";
 import FormRow from "../components/FormRow";
 import FormSelect from "../components/FormSelect";
+import axios from "axios";
+
 const Register = () => {
+  const [owner, setOwner] = useState({});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userInfo = {
+      firstName: e.target[0].value,
+      lastName: e.target[1].value,
+      email: e.target[2].value,
+      address: e.target[3].value,
+      phoneNumber: e.target[4].value,
+      age: e.target[5].value,
+      gender: e.target[6].value,
+      password: e.target[7].value,
+      role: "owner",
+    };
+    register(userInfo);
+  };
+
+  const register = async (userInfo) => {
+    try {
+      const { data } = await axios.post("/auth/register", userInfo);
+      const { owner, token } = data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="bg-gray-200 min-h-screen">
       <div className="flex flex-col justify-center items-center h-full max-w-xl mx-auto">
         <h1 className="font-bold text-4xl text-gray-800 font-mono text-center my-4 py-5">
           Register
         </h1>
-        <form className="flex flex-row bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4 hover:shadow-xl flex-wrap">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-row bg-white shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4 hover:shadow-xl flex-wrap"
+        >
           <FormRow labelName="first name" type="text" />
           <FormRow labelName="last name" type="text" />
           <FormRow labelName="email" type="email" />
@@ -23,7 +53,7 @@ const Register = () => {
           <div className="flex items-center justify-center">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               Sign Up
             </button>
