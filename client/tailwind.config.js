76e0185a7e6951b2a65a9ad4a6e7bb22f26016b6
sonam_rinzin <sonamrinzingurung@gmail.com>
2022-12-23 23:27:51 +0545

    Font and color config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#68904D",
+        primaryLight: "#8ABF6F",
+        primaryDark: "#14471E",
+        secondary: "#EE9B01",
+        tertiary: "#DA6A00",
+      },
+    },
+    fontFamily: {
+      serif: ["Cabin", "sans-serif"],
+      display: ["Roboto Condensed", "sans-serif"],
+    },
   },
   plugins: [],
 };
