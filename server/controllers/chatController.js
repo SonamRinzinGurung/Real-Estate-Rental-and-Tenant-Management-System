552b3cdc66f6f4c5ea4457601ad00a5ba67fc988
sonam_rinzin <sonamrinzingurung@gmail.com>
2023-06-02 11:42:20 +0545

    Message routes and controllers

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -1,4 +1,6 @@
 import Chat from "../models/Chats.js";
+import Messages from "../models/Messages.js";
+import { BadRequestError } from "../request-errors/index.js";
 
 /**
  * @description Start Chat
@@ -16,7 +18,7 @@ const startChat = async (req, res) => {
   });
   if (chatExists) {
     return res
-      .status(200)
+      .status(400)
       .json({ chat: chatExists, msg: "Chat already exists" });
   }
 
@@ -37,4 +39,43 @@ const getChat = async (req, res) => {
   });
   res.status(200).json({ chat });
 };
-export { startChat, getChat };
+
+/**
+ * @description Send message
+ * @returns {object} message
+ */
+const sendMessage = async (req, res) => {
+  const { chatId, message } = req.body;
+  const { userId: senderId } = req.user;
+
+  //check if chat exists
+  const chatExists = await Chat.findById(chatId);
+
+  if (!chatExists) throw new BadRequestError("Chat does not exists");
+
+  //check if user is part of the chat
+  const userInChat = chatExists.chatUsers.includes(senderId);
+
+  if (!userInChat) throw new BadRequestError("User not part of the chat");
+
+  const messageSent = await Messages.create({
+    chatId,
+    senderId,
+    message,
+  });
+  res.status(201).json({ messageSent });
+};
+
+/**
+ * @description Get all messages for a chat
+ * @returns {object} message
+ */
+const getMessages = async (req, res) => {
+  const { chatId } = req.params;
+
+  const messages = await Messages.find({ chatId });
+
+  res.status(200).json({ messages });
+};
+
+export { startChat, getChat, sendMessage, getMessages };
--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -5,7 +5,12 @@ import {
   authorizeTenantUser,
 } from "../middleware/userAuthorization.js";
 
-import { startChat, getChat } from "../controllers/chatController.js";
+import {
+  startChat,
+  getChat,
+  sendMessage,
+  getMessages,
+} from "../controllers/chatController.js";
 
 /**
  * @description start new chat for owner
@@ -14,21 +19,44 @@ import { startChat, getChat } from "../controllers/chatController.js";
 router.post("/owner/start-chat", authorizeOwnerUser, startChat);
 
 /**
- * @description start new chat for tenant
- * @route POST /api/chat/tenant/start-chat
- */
-router.post("/tenant/start-chat", authorizeTenantUser, startChat);
-
-/**
- * @description get all chats
+ * @description get all chats for owner user
  * @route GET /api/chat/owner/get-chat
  */
 router.get("/owner/get-chat", authorizeOwnerUser, getChat);
 
 /**
- * @description get all chats
+ * @description send message route for owner user
+ * @route POST /api/chat/owner/send-message
+ */
+router.post("/owner/send-message", authorizeOwnerUser, sendMessage);
+
+/**
+ * @description get all messages route for owner user
+ * @route GET /api/chat/owner/get-messages/:chatId
+ */
+router.get("/owner/get-messages/:chatId", authorizeOwnerUser, getMessages);
+
+/**
+ * @description start new chat for tenant user
+ * @route POST /api/chat/tenant/start-chat
+ */
+router.post("/tenant/start-chat", authorizeTenantUser, startChat);
+/**
+ * @description get all chats for tenant user
  * @route GET /api/chat/tenant/get-chat
  */
 router.get("/tenant/get-chat", authorizeTenantUser, getChat);
+
+/**
+ * @description send message route for tenant user
+ * @route POST /api/chat/tenant/send-message
+ */
+router.post("/tenant/send-message", authorizeTenantUser, sendMessage);
+
+/**
+ * @description get all messages route for tenant user
+ * @route GET /api/chat/tenant/get-messages/:chatId
+ */
+router.get("/tenant/get-messages/:chatId", authorizeTenantUser, getMessages);
 
 export default router;
