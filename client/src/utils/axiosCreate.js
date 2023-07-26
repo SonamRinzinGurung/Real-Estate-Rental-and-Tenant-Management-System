689043a00356d382b1899f45815c0d5c93cb667f
sonam_rinzin <sonamrinzingurung@gmail.com>
2023-07-26 10:53:30 +0545

    Redirect all to Index page

--- client/src/utils/axiosCreate.js
+++ client/src/utils/axiosCreate.js
@@ -7,7 +7,7 @@ export const injectStore = (_store) => {
 };
 
 const axiosFetch = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL || "/api",
   withCredentials: true,
 });
 
